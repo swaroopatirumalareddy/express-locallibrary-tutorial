c444bdae1cc58a613c100f5adf422664a8919e99
Swaroopatirumalareddy <S538426@NWMISSOURI.EDU>
2020-04-17 15:17:45 -0500

    updated controllers/authorController.js

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -2,7 +2,7 @@ const Author = require('../models/author');
 
 // Display list of all Authors.
 exports.author_list = function(req, res) {
-    res.send('Hello all: Swaroopa Tirumalareddy');
+    res.send('Hello all Swaroopa Tirumalareddy: Author list');
 };
 
 // Display detail page for a specific Author.
