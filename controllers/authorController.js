1d582af50db4f6e6ba31b43a4021a5daa478111d
Swaroopatirumalareddy <S538426@NWMISSOURI.EDU>
2020-05-01 14:09:17 -0500

    updated authorController.js and bookController.js and index.pug

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -30,9 +30,21 @@ exports.author_delete_post = function(req, res) {
     res.send('NOT IMPLEMENTED: Author delete POST');
 };
 
-// Display Author update form on GET.
-exports.author_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author update GET');
+var debug = require('debug')('author');
+
+// Display Author update form on GET
+exports.author_update_get = function(req, res, next) {   
+    
+    req.sanitize('id').escape().trim();
+    Author.findById(req.params.id, function(err, author) {
+        if (err) {
+            debug('update error:' + err);
+            return next(err);
+        }
+        //On success
+        res.render('author_form', { title: 'Update Author', author: author });
+    });
+
 };
 
 // Handle Author update on POST.
